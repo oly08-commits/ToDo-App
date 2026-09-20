26851ec25943046089069a3ac8bb774808732726
Fidel D. Oliveira <fideldeoliveira08@gmail.com>
2026-09-20 06:58:54 +0100

    feat: interface do ToDo App criada!

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// CONSTANTES
+const input = document.querySelector("#input");
+
+const addTask = document.querySelector("#add-btn");
+
+const container = document.querySelector("#list-contaner");
+
+//FUNCAO
+
+// EVENTOS
